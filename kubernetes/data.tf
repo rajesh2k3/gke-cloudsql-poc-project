a5ac6cf7425081a18ed8d432f3670e59f58fd25d
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-07-17 18:24:25 +0530

    Update data.tf

--- kubernetes/data.tf
+++ kubernetes/data.tf
@@ -7,36 +7,36 @@ data "google_client_config" "default" {}
 // get cluster endpoint from cloud storage
 data "google_storage_bucket_object_content" "cluster_endpoint" {
   name   = "cluster-endpoint"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
 
 // get cluster certificate from cloud storage
 data "google_storage_bucket_object_content" "cluster_cert" {
   name   = "cluster-cert"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
 
 
 // get the name of the sql instance database from a cloud storage bucket
 data "google_storage_bucket_object_content" "db" {
   name   = "db"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
 
 // get the name of the sql instance from a cloud storage bucket
 data "google_storage_bucket_object_content" "db_connection" {
   name   = "connection-name"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
 
 // get the name of the sql instance user from a cloud storage bucket
 data "google_storage_bucket_object_content" "db_user" {
   name   = "db_user"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
 
 // get the name of the sql instance user password from a cloud storage bucket
 data "google_storage_bucket_object_content" "db_pass" {
   name   = "db_pass"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
