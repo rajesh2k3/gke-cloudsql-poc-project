9dcf138777c0878285be1d7bcce161ddb6a5a285
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-07-17 16:20:30 +0530

    Update export.tf

--- gke-cluster/export.tf
+++ gke-cluster/export.tf
@@ -2,14 +2,16 @@
 // export the cluster ca certificate to a cloud storage bucket
 resource "google_storage_bucket_object" "cluster_cert" {
   name   = "cluster-cert"
-  content = "${google_container_cluster.sca-cluster.master_auth[0].cluster_ca_certificate}"
-  bucket = "sca-project"
+  content = "${google_container_cluster.ws9kiam-cluster.master_auth[0].cluster_ca_certificate}"
+  #bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
 
 
 // export the cluster endpoint to a cloud storage bucket
 resource "google_storage_bucket_object" "cluster_endpoint" {
   name   = "cluster-endpoint"
-  content = "${google_container_cluster.sca-cluster.endpoint}"
-  bucket = "sca-project"
+  content = "${google_container_cluster.ws9kiam-cluster.endpoint}"
+  #bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
