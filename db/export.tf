a9cc90d6f3d7e62359a1778f0f26e9d05537f904
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-07-17 17:51:01 +0530

    Update export.tf

--- db/export.tf
+++ db/export.tf
@@ -1,25 +1,26 @@
-
 // export the cloud sql instance connection name to a cloud storage bucket
 resource "google_storage_bucket_object" "db_connection" {
   name   = "connection-name"
   content = "${google_sql_database_instance.primary.connection_name}"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
 
 
 // export the database user name to a cloud storage bucket
 resource "google_storage_bucket_object" "db_user" {
-  name   = "db_user"
+  #name   = "db_user"
+  name   = "admin"
   content = "${google_sql_user.admin_user.name}"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
 
 
 // export the database user password to a cloud storage bucket
 resource "google_storage_bucket_object" "db_pass" {
-  name   = "db_pass"
+  #name   = "db_pass"
+  name   = "admin"
   content = "${google_sql_user.admin_user.password}"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
 
 
@@ -27,5 +28,5 @@ resource "google_storage_bucket_object" "db_pass" {
 resource "google_storage_bucket_object" "db" {
   name   = "db"
   content = "${google_sql_database.database.name}"
-  bucket = "sca-project"
+  bucket = "db-ws9kiam-admin"
 }
